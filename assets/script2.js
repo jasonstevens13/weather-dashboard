6ea9737dfb0e06bb17cee96f6ab8f60ee1143a05
jasonstevens13 <jasonstevens13@yahoo.com>
2020-06-17 19:19:43 -0400

    changed height and width of daily weather cards to allow for date to not wrap

--- assets/script2.js
+++ assets/script2.js
@@ -14,148 +14,143 @@ $(document).ready(function () {
         localStorage.setItem("city", citySearch);
 
         // vars for updating search list in DOM
-        var tr = $("<tr>", { id: "tr" + citySearch });
-        var th = $("<th>", { id: "th" + citySearch, scope: "row" });
-        var td = $("<td>", { id: "td" + citySearch });
+        var article = $("<article>", { class: "tile is-child box", data- id: });
+    var th = $("<th>", { id: "th" + citySearch, scope: "row" });
+    var td = $("<td>", { id: "td" + citySearch });
 
-        // updates search list here
-        $("#searchList").prepend(tr);
-        $("#tr" + citySearch).append(th);
-        $("#tr" + citySearch).append(td);
-        $("#td" + citySearch).text(citySearch);
+    // updates search list here
+    $("#searchList").prepend(tr);
+    $("#tr" + citySearch).append(th);
+    $("#tr" + citySearch).append(td);
+    $("#td" + citySearch).text(citySearch);
 
 
 
-        // api call for current weather
+    // api call for current weather
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        // Parse city
+        console.log(response);
+        var cityResult = response.name;
+        console.log(cityResult);
+
+        // Parse unix date/time stamp and convert it to local date
+        var unixTimeStamp = response.dt;
+        var unixTimeStampJS = unixTimeStamp * 1000;
+        var newDate = new Date(unixTimeStampJS);
+        var date = newDate.toLocaleDateString();
+        console.log(date);
+
+
+        // Parse Kelvin temp and convert to Farenheit
+        var tempKelvin = response.main.temp;
+        var temp2 = ((((tempKelvin - 273.15) * 9) / 5) + 32);
+        var temp1 = (Math.floor(temp2 * 100) / 100);
+        var temp = temp1 + " °F";
+        console.log(temp);
+
+
+        // Parse humidity; add % symbol
+        var humidity = response.main.humidity + " %";
+        console.log(humidity);
+
+        // Parse wind speed
+        var windSpeedMetric = response.wind.speed;
+        var windSpeed2 = ((windSpeedMetric * 0.000621371) * 60);
+        var windSpeed1 = (Math.floor(windSpeed2 * 100) / 100);
+        var windSpeed = windSpeed1 + " MPH";
+
+        // var windSpeed = ((windSpeedMetric * 60) * 0.000621371)
+        console.log(windSpeed);
+
+        // Take Longitude and Latitude and call api for UV Index > then parse UV index
+        var Longitude = response.coord.lon.toString();
+        console.log(Longitude);
+        var Latitude = response.coord.lat.toString();
+        console.log(Latitude);
+
+        //Parse weather icon
+        var weatherIconCurrentRaw = response.weather[0].icon;
+        var weatherIconCurrent = "https://openweathermap.org/img/wn/" + weatherIconCurrentRaw + "@2x.png";
+        console.log(weatherIconCurrent);
+
+
+        // Fill in html with current data
+        $("#currentCity").text(cityResult + "(" + date + ")");
+        $("#currentCity").append($("<img>", { src: weatherIconCurrent }));
+        $("#currentTemp").text("Temperature: " + temp);
+        $("#currentHumidity").text("Humidity: " + humidity);
+        $("#currentWindSpeed").text("Wind Speed: " + windSpeed);
+
+
+
+
+
+
+
+        // second api call to the uv api (must get lon and lat from above data)
+        var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?lat=" + Latitude + "&lon=" + Longitude + "&appid=" + myApiKey;
+
         $.ajax({
-            url: queryURL,
+            url: queryURL2,
             method: "GET"
-        }).then(function (response) {
+        }).then(function (response2) {
 
-            // Parse city
-            console.log(response);
-            var cityResult = response.name;
-            console.log(cityResult);
+            var uvIndex = response2.value;
+            console.log(uvIndex);
 
-            // Parse unix date/time stamp and convert it to local date
-            var unixTimeStamp = response.dt;
-            var unixTimeStampJS = unixTimeStamp * 1000;
-            var newDate = new Date(unixTimeStampJS);
-            var date = newDate.toLocaleDateString();
-            console.log(date);
+            // Fill in htl with uv index
+            $("#currentUV").text("UV Index: " + uvIndex);
 
-
-            // Parse Kelvin temp and convert to Farenheit
-            var tempKelvin = response.main.temp;
-            var temp2 = ((((tempKelvin - 273.15) * 9) / 5) + 32);
-            var temp1 = (Math.floor(temp2 * 100) / 100);
-            var temp = temp1 + " °F";
-            console.log(temp);
-
-
-            // Parse humidity; add % symbol
-            var humidity = response.main.humidity + " %";
-            console.log(humidity);
-
-            // Parse wind speed
-            var windSpeedMetric = response.wind.speed;
-            var windSpeed2 = ((windSpeedMetric * 0.000621371) * 60);
-            var windSpeed1 = (Math.floor(windSpeed2 * 100) / 100);
-            var windSpeed = windSpeed1 + " MPH";
-
-            // var windSpeed = ((windSpeedMetric * 60) * 0.000621371)
-            console.log(windSpeed);
-
-            // Take Longitude and Latitude and call api for UV Index > then parse UV index
-            var Longitude = response.coord.lon.toString();
-            console.log(Longitude);
-            var Latitude = response.coord.lat.toString();
-            console.log(Latitude);
-
-            //Parse weather icon
-            var weatherIconCurrentRaw = response.weather[0].icon;
-            var weatherIconCurrent = "https://openweathermap.org/img/wn/" + weatherIconCurrentRaw + "@2x.png";
-            console.log(weatherIconCurrent);
-
-
-            // Fill in html with current data
-            $("#currentCity").text(cityResult + "(" + date + ")");
-            $("#currentCity").append($("<img>", { src: weatherIconCurrent }));
-            $("#currentTemp").text("Temperature: " + temp);
-            $("#currentHumidity").text("Humidity: " + humidity);
-            $("#currentWindSpeed").text("Wind Speed: " + windSpeed);
+        });
 
 
 
+        var queryURL3 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + Latitude + "&lon=" + Longitude + "&appid=" + myApiKey;
+
+        $.ajax({
+            url: queryURL3,
+            method: "GET"
+        }).then(function (response3) {
+
+            // var forecast = response3;
+            console.log(response3);
+
+            // For loop through first 5 days of 7 day forecast
+            for (let i = 0; i < (response3.daily.length - 2); i++) {
+
+                var day = response3.daily[i];
+                var date1 = day.dt;
+                var unixTimeStamp = date1;
+                var unixTimeStampJS = unixTimeStamp * 1000;
+                var newDate = new Date(unixTimeStampJS);
+                var date = newDate.toLocaleDateString();
+                var dateTempKelvin = day.temp.day;
+                var temp2 = ((((dateTempKelvin - 273.15) * 9) / 5) + 32);
+                var temp1 = (Math.floor(temp2 * 100) / 100);
+                var dateTemp = temp1 + " °F";
+                var dateHumidity = day.humidity + " %";
 
 
+                var weatherIconRaw = day.weather[0].icon;
+                var weatherIcon = "https://openweathermap.org/img/wn/" + weatherIconRaw + "@2x.png";
+
+                var htmlElIdDate = "#" + [i] + "date";
+                var htmlElIdDateIcon = "#" + [i] + "dateIcon";
+                var htmlElIdDateTemp = "#" + [i] + "dateTemp";
+                var htmlElIdDateHumidity = "#" + [i] + "dateHumidity";
+
+                $(htmlElIdDate).text(date);
+                $(htmlElIdDateIcon).append($("<img>", { src: weatherIcon }));
+                $(htmlElIdDateTemp).text("Temp: " + dateTemp);
+                $(htmlElIdDateHumidity).text("Humidity: " + dateHumidity);
+
+            }
 
 
-            // second api call to the uv api (must get lon and lat from above data)
-            var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?lat=" + Latitude + "&lon=" + Longitude + "&appid=" + myApiKey;
-
-            $.ajax({
-                url: queryURL2,
-                method: "GET"
-            }).then(function (response2) {
-
-                var uvIndex = response2.value;
-                console.log(uvIndex);
-
-                // Fill in htl with uv index
-                $("#currentUV").text("UV Index: " + uvIndex);
-
-            });
-
-
-
-            var queryURL3 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + Latitude + "&lon=" + Longitude + "&appid=" + myApiKey;
-
-            $.ajax({
-                url: queryURL3,
-                method: "GET"
-            }).then(function (response3) {
-
-                // var forecast = response3;
-                console.log(response3);
-
-                // For loop through first 5 days of 7 day forecast
-                for (let i = 0; i < (response3.daily.length - 2); i++) {
-
-                    var day = response3.daily[i];
-                    var date1 = day.dt;
-                    var unixTimeStamp = date1;
-                    var unixTimeStampJS = unixTimeStamp * 1000;
-                    var newDate = new Date(unixTimeStampJS);
-                    var date = newDate.toLocaleDateString();
-                    var dateTempKelvin = day.temp.day;
-                    var temp2 = ((((dateTempKelvin - 273.15) * 9) / 5) + 32);
-                    var temp1 = (Math.floor(temp2 * 100) / 100);
-                    var dateTemp = temp1 + " °F";
-                    var dateHumidity = day.humidity + " %";
-
-
-                    var weatherIconRaw = day.weather[0].icon;
-                    var weatherIcon = "https://openweathermap.org/img/wn/" + weatherIconRaw + "@2x.png";
-
-                    var htmlElIdDate = "#" + [i] + "date";
-                    var htmlElIdDateIcon = "#" + [i] + "dateIcon";
-                    var htmlElIdDateTemp = "#" + [i] + "dateTemp";
-                    var htmlElIdDateHumidity = "#" + [i] + "dateHumidity";
-
-                    $(htmlElIdDate).text(date);
-                    $(htmlElIdDateIcon).append($("<img>", { src: weatherIcon }));
-                    $(htmlElIdDateTemp).text("Temp: " + dateTemp);
-                    $(htmlElIdDateHumidity).text("Humidity: " + dateHumidity);
-
-                }
-
-
-
-
-
-
-            });
 
 
 
@@ -165,10 +160,15 @@ $(document).ready(function () {
 
 
 
-
-
-
     });
+
+
+
+
+
+
+
+});
 
 
 
